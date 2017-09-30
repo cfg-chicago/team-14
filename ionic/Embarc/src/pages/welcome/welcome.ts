231a152170b2e6f8b0a9d693716addbd5534eaec
Pavan Purewal <pavanpurewal0@gmail.com>
2017-09-30 01:55:13 -0700

    ionic platform update

---
+++ ionic/Embarc/src/pages/welcome/welcome.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController } from 'ionic-angular';
+
+/**
+ * The Welcome Page is a splash page that quickly describes the app,
+ * and then directs the user to create an account or log in.
+ * If you'd like to immediately put the user onto a login/signup page,
+ * we recommend not using the Welcome page.
+*/
+@IonicPage()
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html'
+})
+export class WelcomePage {
+
+  pleaseWait: string;
+
+  constructor(public navCtrl: NavController) { }
+
+  login() {
+    this.pleaseWait = "Loading...";
+    setTimeout(()=>{
+      this.pleaseWait = "";
+      this.navCtrl.push('LoginPage');
+    }, 1000);
+
+
+  }
+
+  signup() {
+    this.navCtrl.push('SignupPage');
+  }
+}
